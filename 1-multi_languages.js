67a545a5dd301061f2268fb9800ba97ff2a0afb5
Teye Isaac <teyeisaacnomo10@gmail.com>
2025-11-09 08:35:25 +0000

    Add multi-language printing script

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+// Declare constant variables for each line
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+// Print each line
+console.log(line1);
+console.log(line2);
+console.log(line3);
